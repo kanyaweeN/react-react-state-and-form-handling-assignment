a7958795308685c96ab4755e41081714b68d776e
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-08-13 15:46:05 +0700

    feat:add answer

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,54 @@
+import { useState } from 'react'
+
 function ProductForm() {
+  const [product, setProduct] = useState({
+    name: "",
+    image: "",
+    price: 0,
+    description: "",
+    email: "",
+  })
+  const [error, setError] = useState({
+    name: "",
+    image: "",
+    price: "",
+    description: "",
+    email: "",
+  })
+
+  // console.log(JSON.stringify(product));
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setProduct({
+      ...product,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let err = {}
+    if (!product.name.trim()) {
+      err.name = "Name is required.";
+    }
+    if (!product.image.trim()) {
+      err.image = "Image is required.";
+    }
+    if (!product.price) {
+      err.price = "Price is required.";
+    }
+    if (!product.description.trim()) {
+      err.description = "Description is required.";
+    }
+    if (!product.email.trim()) {
+      err.email = "Email is required";
+    }
+    setError(err);
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,9 +58,11 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            value={product.name}
+            onChange={(e) => handleInputChange(e)}
           />
         </label>
+        {error.name && <p className="error-message">{error.name}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -22,9 +72,11 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            value={product.image}
+            onChange={(e) => handleInputChange(e)}
           />
         </label>
+        {error.image && <p className="error-message">{error.image}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -34,9 +86,11 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            // value={product.price}
+            onChange={(e) => handleInputChange(e)}
           />
         </label>
+        {error.price && <p className="error-message">{error.price}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -46,11 +100,13 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            value={product.description}
+            onChange={(e) => handleInputChange(e)}
             rows={4}
             cols={30}
           />
         </label>
+        {error.description && <p className="error-message">{error.description}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -60,9 +116,11 @@ function ProductForm() {
             name="email"
             type="email"
             placeholder="Enter your email here"
-            onChange={() => {}}
+            value={product.email}
+            onChange={(e) => handleInputChange(e)}
           />
         </label>
+        {error.email && <p className="error-message">{error.email}</p>}
       </div>
       <div className="form-actions">
         <button type="submit">Create</button>
